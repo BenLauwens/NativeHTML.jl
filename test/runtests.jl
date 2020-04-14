07acba47a90bac05445bdeda4bb7d90a4e6dba93
Ben Lauwens <ben.lauwens@gmail.com>
2020-04-14 22:25:22 +0200

    Complete README.adoc

--- test/runtests.jl
+++ test/runtests.jl
@@ -60,3 +60,18 @@ frag = fragment() do
 end
 
 display(frag)
+
+page = html() do
+    head() do
+        title("Hello, World!")
+    end
+    body() do
+        div() do
+            span("Hello", style="color:red;")
+            span(", ")
+            b("World!")
+        end
+    end
+end
+
+display(page)
