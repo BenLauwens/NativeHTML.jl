1e71a725242efaeb793992d8d4ef18561b5b6d9d
Ben Lauwens <ben.lauwens@gmail.com>
2020-04-14 22:56:46 +0200

    modify README.adoc

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,61 +1,10 @@
 using NativeHTML
 
-const email = "ben.lauwens@rma.ac.be"
-
-page = html() do
-    head() do
-        meta(charset="utf-8")
-        meta(name="viewport", content="width=device-width, initial-scale=1")
-        title("Test")
-    end
-    body(style="background-color: white;") do
-        header(id="header") do
-            div() do
-                a(href="/", rel="noopener") do
-                    img(src="/static/logo-rma.jpg")
-                    span("Management Software")
-                end
-            end
-            div() do
-                a(class="icon") do
-                    i(class="fas fa-user")
-                end
-                a(class="icon") do
-                    i(class="fas fa-toolbox")
-                end
-            end
-            div() do
-                if email !== nothing
-                    a(email, href="/users/profile")
-                    a(class="icon", href="/logout") do
-                        i(class="fas fa-sign-out-alt")
-                    end
-                else
-                    a(class="icon", href="/login") do
-                        i(class="fas fa-sign-in-alt")
-                    end
-                end
-            end
-        end
-    end
-end
-
 frag = fragment() do
     div() do
-        span("Test", style="color: red;")
-        p() do
-            text("Dit is een Test.")
-            b("In vet!")
-            text("Dit is nog een Test.")
-        end
-        div() do
-            span("Test", style="color: red;")
-            p() do
-                text("Dit is een Test.")
-                b("In vet!")
-                text("Dit is nog een Test.")
-            end
-        end
+        span("Hello", style="color:red;")
+        text(", ")
+        b("World!")
     end
 end
 
@@ -68,7 +17,7 @@ page = html() do
     body() do
         div() do
             span("Hello", style="color:red;")
-            span(", ")
+            text(", ")
             b("World!")
         end
     end
